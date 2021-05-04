5fb1b52b33267b1bb0d47fbcf86a40ccb7dddc98
Vitali Breida <vitali.breida@sap.com>
2021-05-04 17:15:59 +0300

    Redux state is transferred from server to client

--- src/App.js
+++ src/App.js
@@ -8,24 +8,25 @@ import HomePage from "./app/Pages/HomePage/HomePage";
 import { Provider } from "react-redux";
 import { StrictMode } from "react";
 
-const App = (Router, location, context, store) => (
-  <StrictMode>
-    <Provider store={store}>
-      <Router>
-        <AddMovieDialog />
-        <EditMovieDialog />
-        <DeleteMovieDialog />
+const App = ({Router, location, context, store}) => {
+  return (
+    <StrictMode>
+      <Provider store={store}>
+        <Router location={location} context={context}>
+          <AddMovieDialog />
+          <EditMovieDialog />
+          <DeleteMovieDialog />
 
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/search/:keyword" component={HomePage} />
-          <Route path="/film/:id" component={HomePage} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </Router>
-    </Provider>
-  </StrictMode>
-);
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/search/:keyword" component={HomePage} />
+            <Route path="/film/:id" component={HomePage} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        </Router>
+      </Provider>
+    </StrictMode>)
+}
 
 App.defaultProps = {
   location: null,
